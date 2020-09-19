77864457503ee20df67954873d069f8c02910383
Rubens <rubao.ma@gmail.com>
2020-09-19 12:46:57 -0300

    implementacao de calculo de gorjeta e formato de exibicao em duas casas decimais

--- app/src/main/java/com/example/calculadoradegorgeta/MainActivity.java
+++ app/src/main/java/com/example/calculadoradegorgeta/MainActivity.java
@@ -2,13 +2,89 @@ package com.example.calculadoradegorgeta;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editValor;
+    private TextView textPorcentagem;
+    private TextView textGorjeta;
+    private TextView textTotal;
+    private SeekBar seekBarGorjeta;
+
+    private double porcentagem = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editValor = findViewById(R.id.editValor);
+        textGorjeta = findViewById(R.id.textGorjeta);
+        textPorcentagem = findViewById(R.id.textPorcentagem);
+        textTotal = findViewById(R.id.textTotal);
+        seekBarGorjeta = findViewById(R.id.seekBarGorjeta);
+
+
+        //add listener SeekBar
+        seekBarGorjeta.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                porcentagem = i;
+                textPorcentagem.setText(Math.round(porcentagem) + "%");
+                calcular();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
+
+    public void calcular() {
+        String valorRecuperado = editValor.getText().toString();
+        if (valorRecuperado == null || valorRecuperado.equals("")) {
+
+            Toast.makeText(
+                    getApplicationContext(),
+                    "Digite um valor primeiro",
+                    Toast.LENGTH_SHORT
+            ).show();
+
+        } else {
+            //Convert string to double
+
+            double valorDigitado = Double.parseDouble(valorRecuperado);
+
+            //calc total tip
+            double gorjeta = valorDigitado * (porcentagem / 100);
+            double totalConta = gorjeta + valorDigitado;
+
+            //standard value format
+            DecimalFormat decimalFormat = new DecimalFormat("#,##0.00");
+            decimalFormat.setRoundingMode(RoundingMode.DOWN);
+
+            // display results
+            textGorjeta.setText("R$ " + decimalFormat.format(gorjeta));
+            textTotal.setText("R$ " + decimalFormat.format(totalConta));
+
+
+        }
+    }
+
 }
